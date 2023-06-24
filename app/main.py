176f51cfb4039710eaa21dab13749e286c626499
Hikari <dev@schoash.com>
2023-06-24 08:34:02 +0200

    feat: added api gateway

--- app/main.py
+++ app/main.py
@@ -1,17 +1,40 @@
-from flask import Flask, request, jsonify
+import requests
+from flask import Flask, request, jsonify, json
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
+from flask_caching import Cache
+
+ALF_API_URL = "https://services1.fmh.de/webapi/banken-portal/graphql"
+ALF_API_TOKEN = "JlrIZRj87Ozg30leAlPxBHIaqtTphjyc"
 
 app = Flask(__name__)
 CORS(app)
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["2 per minute", "1 per second"],
+    default_limits=["10 per minute", "1 per second"],
     storage_uri="memory://",
     strategy="fixed-window",  # or "moving-window"
 )
+
+
+@app.route('/certificates', methods=['POST'])
+@cache.cached(timeout=3000)
+def get_certificates():
+    data = request.data
+    requests_response = requests.post(
+        url=ALF_API_URL,
+        headers={
+            "ApiKey": ALF_API_TOKEN,
+            "Content-Type": "application/json",
+        },
+        data=request.data,
+    )
+
+    return requests_response.content, 200, {"Content-Type": "text/json"}
 
 
 @app.route('/send_email', methods=['POST'])
@@ -47,11 +70,6 @@ def send_email():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
-@app.route('/')
-def hello_world():
-    return jsonify({'msg': 'Hello test World!!!'})
 
 
 if __name__ == '__main__':
